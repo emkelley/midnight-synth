69812c76869e1e94ffdd61604d5591b8e0207ee5
Eric Kelley <hello@ericmkelley.com>
2021-03-26 21:40:19 -0400

    stuff

---
+++ examples/example.js
@@ -0,0 +1,35 @@
+const { default: axios } = require("axios");
+const ID = process.env.SPOTIFY_CLIENT_ID;
+const SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const authBuffer = Buffer.from(`${ID}:${SECRET}`).toString("base64");
+
+module.exports = {
+  name: "song",
+  aliases: ["music"],
+  run: async (client, channel, userstate, context) => {
+    axios({
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      headers: { Authorization: `Basic ${authBuffer}` },
+      params: {
+        grant_type: "refresh_token",
+        refresh_token: process.env.SPOTIFY_AUTH_REFRESH_TOKEN,
+      },
+    }).then(({ data: { access_token } }) =>
+      axios({
+        method: "GET",
+        url: "https://api.spotify.com/v1/me/player/currently-playing?",
+        headers: { Authorization: "Bearer " + access_token },
+      }).then(({ statusText, data: { item } }) => {
+        if (statusText === "No Content") {
+          const notPlaying = "0NEGUY's Spotify is not currently playing music";
+          client.say(channel, notPlaying);
+        } else {
+          // item.external_urls.spotify
+          const message = `catJAM We're listening to: ${item.name} by ${item.artists[0].name}`;
+          client.say(channel, message);
+        }
+      })
+    );
+  },
+};
